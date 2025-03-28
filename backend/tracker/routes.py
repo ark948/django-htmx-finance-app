1539ce565698afc1d2f3df8a60c8af8c7c406b18
ark948 <armankarimi948@gmail.com>
2025-03-28 23:24:49 +0330

    Transaction list working ok, some problem with TailwindCSS or DaisyUI

--- backend/tracker/routes.py
+++ backend/tracker/routes.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'tracker'
 urlpatterns = [
+    path("transactions/", views.transactions_list, name='transactions-list'),
     path("", views.index, name='index'),
 ]
--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.decorators import login_required
+
+
+from .models import Transaction
+from .filters import TransactionFilter
 
 # Create your views here.
 
 def index(request: HttpRequest) -> HttpResponse:
     response = render(request, "tracker/index.html", {})
+    return response
+
+
+
+@login_required
+def transactions_list(request):
+    transaction_filter = TransactionFilter(
+        request=request.GET,
+        queryset=Transaction.objects.filter(user=request.user).select_related('category')
+    )
+    context = {'filter': transaction_filter}
+    if request.htmx:
+        response = render(request, "tracker/partials/transactions-container.html", context)
+        return response
+    response = render(request, "tracker/transactions-list.html", context)
     return response
