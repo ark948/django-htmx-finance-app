4e0f50ddcd8710c70ede2bbd41e8e34c06520abb
ark948 <armankarimi948@gmail.com>
2025-03-28 18:36:41 +0330

    Added User model to admin

--- backend/tracker/admin.py
+++ backend/tracker/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Transaction, Category
+from .models import Transaction, Category, User
 
 
 admin.site.register(Transaction)
 admin.site.register(Category)
+admin.site.register(User)
