ace084a72bdcf715600d60a3cfb94ee3edc804fa
ark948 <armankarimi948@gmail.com>
2025-04-02 18:24:11 +0330

    Tests for date filter

--- backend/tracker/tests/test_views.py
+++ backend/tracker/tests/test_views.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime, timedelta
 from django.test.client import Client
 from django.urls import reverse
 
@@ -17,3 +18,55 @@ def test_total_values_appear_on_list_page(user_transactions, client: Client):
     assert response.context['total_income'] == income_total
     assert response.context['total_expenses'] == expense_total
     assert response.context['net_income'] == net
+
+
+@pytest.mark.django_db
+def test_transaction_type_filter(user_transactions, client: Client):
+    user = user_transactions[0].user
+    client.force_login(user)
+
+    # income check
+    GET_params = {'transaction_type': 'income'}
+    resposne = client.get(reverse('tracker:transactions-list'), GET_params)
+
+    qs = resposne.context['filter'].qs
+
+    for transaction in qs:
+        assert transaction.type == 'income'
+
+
+    # expense check
+    GET_params = {'transaction_type': 'expense'}
+    resposne = client.get(reverse('tracker:transactions-list'), GET_params)
+
+    qs = resposne.context['filter'].qs
+
+    for transaction in qs:
+        assert transaction.type == 'expense'
+
+
+@pytest.mark.django_db
+def test_transaction_start_end_date_filter(user_transactions, client: Client):
+    user = user_transactions[0].user
+    client.force_login(user)
+
+    start_date_cutoff = datetime.now().date() - timedelta(days=120)
+    GET_params = {'start_date': start_date_cutoff}
+    resposne = client.get(reverse('tracker:transactions-list'), GET_params)
+
+    qs = resposne.context['filter'].qs
+
+    for transaction in qs:
+        assert transaction.date >= start_date_cutoff
+
+    end_date_cutoff = datetime.now().date() - timedelta(days=20)
+    GET_params = {'end_date': end_date_cutoff}
+    resposne = client.get(reverse('tracker:transactions-list'), GET_params)
+
+    qs = resposne.context['filter'].qs
+
+    for transaction in qs:
+        assert transaction.date <= end_date_cutoff
+
+
+# write a test for both start date and end date (exercise)
