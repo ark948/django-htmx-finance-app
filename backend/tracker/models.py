9a8216cd2327d8964d0aebf8e4892e7c8bfe3bcf
ark948 <armankarimi948@gmail.com>
2025-04-13 12:39:09 +0330

    Encrypting amount field in database

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from encrypted_fields.fields import EncryptedFieldMixin
+from encrypted_fields.fields import EncryptedFloatField
+
 
 from .managers import TransactionQuerySet
 
@@ -27,7 +30,7 @@ class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     type = models.CharField(max_length=7, choices=TRANSACTION_TYPE_CHOICES)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = EncryptedFloatField()
     date = models.DateField()
 
     # custom manager
